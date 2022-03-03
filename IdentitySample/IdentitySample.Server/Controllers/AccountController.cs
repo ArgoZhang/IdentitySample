3b1baf70e92ae206fc38edc2152eccf5e6d58f92
Argo-Tianyi <argo@163.com>
2022-03-03 15:46:44 +0800

    init: 初始化仓库

---
+++ IdentitySample/IdentitySample.Server/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace IdentitySample.Server.Controllers;
+
+/// <summary>
+/// 
+/// </summary>
+[AllowAnonymous]
+public class AccountController : Controller
+{
+    [HttpPost]
+    public async Task<IActionResult> Login(string? userName, string? password)
+    {
+        // check username and password
+        var auth = true;
+        return auth ? await SignInAsync(userName ?? "admin") : Redirect("/Account/Login");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync();
+        return Redirect("/");
+    }
+
+    private async Task<IActionResult> SignInAsync(string userName, string authenticationScheme = CookieAuthenticationDefaults.AuthenticationScheme)
+    {
+        var identity = new ClaimsIdentity(authenticationScheme);
+        identity.AddClaim(new Claim(ClaimTypes.Name, userName));
+
+        var properties = new AuthenticationProperties();
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity), properties);
+
+        return Redirect("/");
+    }
+}
---
+++ IdentitySample/IdentitySample.Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using IdentitySample.Shared.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddBootstrapBlazor();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
+builder.Services.AddAuthorization();
+
+builder.Services.AddSingleton<WeatherForecastService>();
+
+// 增加 Table 数据服务操作类
+builder.Services.AddTableDemoDataService();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// add this ...
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
